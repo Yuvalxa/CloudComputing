e5780b2a10ccf69683433a79854200ecba8eecd1
YuvalM <yuvalsmastey@gmail.com>
2024-01-19 13:05:15 +0200

    added all endpoints

--- usersService/src/main/java/il/ac/afeka/usersservice/ReactiveUsersController.java
+++ usersService/src/main/java/il/ac/afeka/usersservice/ReactiveUsersController.java
@@ -1,14 +1,17 @@
+package il.ac.afeka.usersservice;
 
 import il.ac.afeka.usersservice.UsersService;
 import il.ac.afeka.usersservice.boundaries.UserBoundary;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping(path = {"/messages"})
+@RequestMapping(path = {"/users"})
 public class ReactiveUsersController {
 	private UsersService usersService;
 	
@@ -17,19 +20,37 @@ public class ReactiveUsersController {
 	}
 	
 	@GetMapping(
-		produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
+			produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
 	public Flux<UserBoundary> getAllUsers (){
 		return this.usersService
 			.getAll()
 			.log();
 	}
+	@PostMapping(
+			consumes = {MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Mono<UserBoundary> createMessage (
+			@RequestBody UserBoundary user) {
+		return this.usersService
+				.createUser(user)
+				.log();
+	}
+	public Mono<UserBoundary> getUserByEmailAndPassword(
+			@PathVariable("email") String email,
+			@RequestParam(name = "password") String password) {
 
+		return this.usersService.GetUserByEmail(email, password)
+				.log();
+	}
 	@GetMapping(
-			path = "/users",
 			produces = {MediaType.TEXT_EVENT_STREAM_VALUE})
 	public Flux<UserBoundary> getUsers(
 				@PathVariable(name = "criteria") String criteria,
 				@PathVariable(name = "value") String value) {
+		return getUsersByCitria(criteria, value);
+	}
+
+	private Flux<UserBoundary> getUsersByCitria(String criteria, String value) {
 		switch (criteria) {
 			case "byLastname":
 				return this.usersService.getUsersbyLastname(value)
@@ -40,13 +61,14 @@ public class ReactiveUsersController {
 			case "byDomain":
 				return this.usersService.getUsersbyDomain(value)
 						.log();
-			default: // todo return empty Mono
-			return this.usersService.getUsersbyDomain(value)
-					.log();
-		}
+			default:
+				return Flux.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Criteria not found"));
+			}
 	}
+
 	@DeleteMapping
 	public Mono<Void> deleteAll(){
+
 		return this.usersService
 			.deleteAll()
 			.log();
