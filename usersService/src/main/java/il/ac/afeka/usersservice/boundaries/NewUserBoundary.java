c4e9aa0b6f8e4b46ca2d67c9421833ae76f3c391
Noam Levy <noam5003@gmail.com>
2024-01-20 11:01:09 +0200

    requested changes

--- usersService/src/main/java/il/ac/afeka/usersservice/boundaries/NewUserBoundary.java
+++ usersService/src/main/java/il/ac/afeka/usersservice/boundaries/NewUserBoundary.java
@@ -1,54 +1,9 @@
 package il.ac.afeka.usersservice.boundaries;
 
-public class NewUserBoundary {
-    private UserBoundary user;
+public class NewUserBoundary extends UserBoundary {
     private String password;
 
     public NewUserBoundary() {}
-
-    public void setUser(UserBoundary user) {
-        this.user = user;
-    }
-
-    public NameBoundary getName() {
-        return user.getName();
-    }
-
-    public void setName(NameBoundary name) {
-        this.user.setName(name);
-    }
-
-    public String getEmail() {
-        return this.user.getEmail();
-    }
-
-    public void setEmail(String email) {
-        this.user.setEmail(email);
-    }
-
-    public String getBirthdate() {
-        return this.user.getBirthDate();
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.user.setBirthDate(birthdate);
-    }
-
-    public String getRecruitDate() {
-        return this.user.getRecruitDate();
-    }
-
-    public void setRecruitDate(String recruitDate) {
-        this.user.setRecruitDate(recruitDate);
-    }
-
-    public String[] getRoles() {
-        return this.user.getRoles();
-    }
-
-    public void setRoles(String[] roles) {
-        this.user.setRoles(roles);
-    }
 
     public String getPassword() {
         return password;
@@ -60,9 +15,10 @@ public class NewUserBoundary {
 
     @Override
     public String toString() {
-        return "NewUserBoundary {" +
-                "user=" + user +
-                ", password='" + password + '\'' +
+        String userBoundaryString = super.toString();
+        return "New" +
+                userBoundaryString.substring(0, userBoundaryString.length() - 1)  + // removes '}' in parent's toString()
+                ", password=" + this.password +
                 '}';
     }
 }
