b826626cd16d6b5358ec3355bcf9c4b155861800
YuvalM <yuvalsmastey@gmail.com>
2024-01-21 18:18:58 +0200

    added new User Boundry rule on create user

--- usersService/src/main/java/il/ac/afeka/usersservice/logic/ReactiveUsersService.java
+++ usersService/src/main/java/il/ac/afeka/usersservice/logic/ReactiveUsersService.java
@@ -1,5 +1,6 @@
 package il.ac.afeka.usersservice.logic;
 
+import il.ac.afeka.usersservice.boundaries.NewUserBoundary;
 import il.ac.afeka.usersservice.boundaries.UserBoundary;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -7,58 +8,58 @@ import reactor.core.publisher.Mono;
 
 @Service
 public class ReactiveUsersService implements UsersService {
-	private ReactiveUserCrud userCrud;
+    private ReactiveUserCrud userCrud;
 
-	public ReactiveUsersService(ReactiveUserCrud userCrud) {
-		this.userCrud = userCrud;
-	}
+    public ReactiveUsersService(ReactiveUserCrud userCrud) {
+        this.userCrud = userCrud;
+    }
 
-	@Override
-	public Flux<UserBoundary> getUsersByDomain(String domain) {
-		return this.userCrud
-				.findByEmailEndingWith(domain)
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Flux<UserBoundary> getUsersByDomain(String domain) {
+        return this.userCrud
+                .findByEmailEndingWith(domain)
+                .map(UserBoundary::new);
+    }
 
-	@Override
-	public Flux<UserBoundary> getUsersByMinimumAge(int minimumAgeInYears) {
-		return this.userCrud
-				.findAll()
-				.filter(user -> user.calculateAge() >= minimumAgeInYears)
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Flux<UserBoundary> getUsersByMinimumAge(int minimumAgeInYears) {
+        return this.userCrud
+                .findAll()
+                .filter(user -> user.calculateAge() >= minimumAgeInYears)
+                .map(UserBoundary::new);
+    }
 
-	@Override
-	public Flux<UserBoundary> getUsersByLastname(String lastname) {
-		return userCrud
-				.findAllByLastname(lastname)
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Flux<UserBoundary> getUsersByLastname(String lastname) {
+        return userCrud
+                .findAllByLastname(lastname)
+                .map(UserBoundary::new);
+    }
 
-	@Override
-	public Mono<Void> deleteAll() {
-		return this.userCrud.deleteAll();
-	}
+    @Override
+    public Mono<Void> deleteAll() {
+        return this.userCrud.deleteAll();
+    }
 
-	@Override
-	public Flux<UserBoundary> getAll() {
-		return userCrud
-				.findAll()
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Flux<UserBoundary> getAll() {
+        return userCrud
+                .findAll()
+                .map(UserBoundary::new);
+    }
 
-	@Override
-	public Mono<UserBoundary> GetUserByEmail(String email, String password) {
-		return userCrud
-				.findByEmailAndPassword(email, password)
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Mono<UserBoundary> GetUserByEmail(String email, String password) {
+        return userCrud
+                .findByEmailAndPassword(email, password)
+                .map(UserBoundary::new);
+    }
 
-	@Override
-	public Mono<UserBoundary> createUser(UserBoundary user) {
-		return Mono.just(user)
-				.map(UserBoundary::toEntity)
-				.flatMap(this.userCrud::save)
-				.map(UserBoundary::new);
-	}
+    @Override
+    public Mono<UserBoundary> createUser(NewUserBoundary user) {
+        return Mono.just(user)
+                .map(NewUserBoundary::toEntity)
+                .flatMap(this.userCrud::save)
+                .map(UserBoundary::new);
+    }
 }
